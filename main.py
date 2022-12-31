dace5a82f0978d0cc9ce786b425e916c56106f8a
Adodoadjie <109514773+Ados1@users.noreply.github.com>
2022-12-31 20:49:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+games_image = [rock, paper, scissors]
+
+users_choice = int(input("Which one would you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
+
+if users_choice >= 3 or users_choice < 0:
+  print("Invalid input")
+else:
+ print(games_image[users_choice])
+ 
+ computer_choice= random.randint(0, 2)
+ print("Computer chose:")
+ print(games_image[computer_choice])
+
+ if users_choice >= 3 or users_choice < 0:
+  print("Invalid input")
+ elif users_choice == 0 and computer_choice ==2:
+  print("You win.")
+ elif users_choice ==2 and computer_choice ==0:
+  print("You lose.")
+ elif users_choice > computer_choice:
+  print("You win.")
+ elif users_choice < computer_choice:
+  print("You lose.")
+ else :
+  print("You draw.")
+  
